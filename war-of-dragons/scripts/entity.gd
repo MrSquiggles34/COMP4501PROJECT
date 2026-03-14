455d24dfc18a2faf83c5ba68d183b8da584b7a46
MrSquiggles <57115950+MrSquiggles34@users.noreply.github.com>
2026-03-14 17:55:29 -0400

    Added models for the dragons & slime, and collectible

--- war-of-dragons/scripts/entity.gd
+++ war-of-dragons/scripts/entity.gd
@@ -9,14 +9,19 @@ enum EntityType { DRAGON, HOSTILE, COLLECTIBLE, OBSTACLE, BASE }
 var is_selected: bool = false
 var outline_material: ShaderMaterial
 
-@onready var mesh: MeshInstance3D = $MeshInstance3D
+@onready var model = $Model
+var meshes: Array[MeshInstance3D] = []
 
 func _ready() -> void:
 	outline_material = ShaderMaterial.new()
 	outline_material.shader = load("res://shaders/entity_outline.gdshader")
+	
+	for child in model.find_children("*", "MeshInstance3D", true):
+		meshes.append(child)
 
 func set_selected(selected: bool) -> void:
-	if selected:
-		mesh.material_overlay = outline_material
-	else:
-		mesh.material_overlay = null
+	for mesh in meshes:
+		if selected:
+			mesh.material_overlay = outline_material
+		else:
+			mesh.material_overlay = null
